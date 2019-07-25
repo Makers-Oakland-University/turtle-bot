b1893bab13d3141a7d75ff2940af2f93ee8f46e0
Dave Stewart <dstewart@knightglobal.com>
2019-07-25 18:05:21 -0400

    Added screen door

--- turtle_eyes/CSS Eyes/js/eyes.js
+++ turtle_eyes/CSS Eyes/js/eyes.js
@@ -1,3 +1,25 @@
+window.onload = function(e){ 
+    makeScreenDoor(75);
+}
+
+function makeScreenDoor(numberOfRows) {
+    var flexRow = document.createElement("div");
+    flexRow.className = 'flex-grow-1';
+    document.getElementById("screenDoor").appendChild(flexRow);
+
+    for (let index = 0; index < numberOfRows; index++) {
+        var darkRow = document.createElement("div");
+        darkRow.className = 'blackRow';
+        darkRow.setAttribute("style", " z-index: 2;");
+
+        var flexRow = document.createElement("div");
+        flexRow.className = 'flex-grow-1';
+
+        document.getElementById("screenDoor").appendChild(darkRow);
+        document.getElementById("screenDoor").appendChild(flexRow);
+    }
+}
+
 function AngryEyes(){
     setTimeout(function(){
         document.getElementById("circleLeft").classList.remove('angryLeft');
