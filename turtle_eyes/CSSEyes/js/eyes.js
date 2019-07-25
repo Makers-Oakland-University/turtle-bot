ae5ee2fe001d1eed3cca3cb5d1c7a0cf5bdff0c0
Dave Stewart <dstewart@knightglobal.com>
2019-07-25 16:10:10 -0400

    Added CSS Eyes

---
+++ turtle_eyes/CSSEyes/js/eyes.js
@@ -0,0 +1,8 @@
+function PushButton(){
+    setTimeout(function(){
+        document.getElementById("circleLeft").classList.remove('errorGlowLeft');
+        document.getElementById("circleRight").classList.remove('errorGlowRight');
+    }, 6000);
+    document.getElementById("circleLeft").classList.add('errorGlowLeft');
+    document.getElementById("circleRight").classList.add('errorGlowRight');
+}
