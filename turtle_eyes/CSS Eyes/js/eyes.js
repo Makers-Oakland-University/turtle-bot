088c8a5e4265138d29164ecc2445b9c6659e78c2
Dave Stewart <dstewart@knightglobal.com>
2019-07-25 17:28:43 -0400

    CSS Eyes Centerd and blink done

--- turtle_eyes/CSS Eyes/js/eyes.js
+++ turtle_eyes/CSS Eyes/js/eyes.js
@@ -1,8 +1,17 @@
 function AngryEyes(){
     setTimeout(function(){
-        document.getElementById("circleLeft").classList.remove('errorGlowLeft');
-        document.getElementById("circleRight").classList.remove('errorGlowRight');
+        document.getElementById("circleLeft").classList.remove('angryLeft');
+        document.getElementById("circleRight").classList.remove('angryRight');
     }, 6000);
-    document.getElementById("circleLeft").classList.add('errorGlowLeft');
-    document.getElementById("circleRight").classList.add('errorGlowRight');
+    document.getElementById("circleLeft").classList.add('angryLeft');
+    document.getElementById("circleRight").classList.add('angryRight');
+}
+
+function Blink(){
+    setTimeout(function(){
+        document.getElementById("circleLeft").classList.remove('blinkLeft');
+        document.getElementById("circleRight").classList.remove('blinkRight');
+    }, 1000);
+    document.getElementById("circleLeft").classList.add('blinkLeft');
+    document.getElementById("circleRight").classList.add('blinkRight');
 }
